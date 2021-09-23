7d329683237960268681f22357e02b5967be2db5
Robert King <robert.o.king@hotmail.co.uk>
2021-09-23 22:16:29 +0200

    Added: Selected <Cell>s get green inline

--- src/components/cell.jsx
+++ src/components/cell.jsx
@@ -1,6 +1,6 @@
 import {ReactComponent as Star} from '../svg/star-solid.svg';
 
-export const Cell = ({ i, j, contents, cellSize, clickHandler }) => {
+export const Cell = ({ i, j, contents, cellSize, clickHandler, selected }) => {
   const handleClick = (event) => {
     event.preventDefault();
     clickHandler(i, j);
@@ -12,6 +12,11 @@ export const Cell = ({ i, j, contents, cellSize, clickHandler }) => {
         x={(i+0.5) * cellSize} y={(j+0.5) * cellSize}
         width={cellSize * 0.66} height={cellSize * 0.66}
       />*/}
+      {selected &&
+          (<rect
+            x={(i * cellSize) + 3} y={(j * cellSize) + 3}
+            width={cellSize - 6} height={cellSize - 6}
+            className="selected" />)}
       <rect
         x={i * cellSize} y={j * cellSize}
         width={cellSize} height={cellSize}
