868ee635fb9b476e7eb525f5ff8b7754e24f5c2d
Robert King <robert.o.king@hotmail.co.uk>
2021-10-05 22:54:31 +0100

    Added: If regions is empty, fill it with a 2D array with necessary rows and columns

--- src/components/regions.jsx
+++ src/components/regions.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 import { create2DArray } from '../helpers/create_2D_array.js';
 
-export const Regions = (props) => {
+const setDefaultRegions = (rows, cols) => {
+  const new2DArray = create2DArray(rows, cols, 0);
+
+  return new2DArray;
+};
+
+export const Regions = ({ rows, cols, regions, setRegions }) => {
+  React.useEffect(() => {
+    if (regions.length === 0) {
+      setRegions(setDefaultRegions(rows, cols));
+    }
+  }, [rows, cols, regions, setRegions])
+
   return (
     <g></g>
   );
