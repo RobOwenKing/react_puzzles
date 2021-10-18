97dc5bda97b9c190b44dcd056bc14f7877c78a77
Robert King <robert.o.king@hotmail.co.uk>
2021-10-18 22:00:11 +0100

    Added: parseStarBattleRows()

--- src/helpers/puzzleToConstraints.js
+++ src/helpers/puzzleToConstraints.js
@@ -1,5 +1,20 @@
+const parseStarBattleRows = (constraints, rows, cols) => {
+  for (let j = 0; j < rows; j += 1) {
+    const rowConstraint = {
+      'type': 'sb_house',
+      'keys': [],
+      'errors': []
+    }
+    for (let i = 0; i < cols; i += 1) {
+      rowConstraint.keys.push(j * cols + i);
+    }
+    constraints.push(rowConstraint);
+  }
+};
+
 const parseStarBattle = (constraints, value, rows, cols, regions) => {
-  console.log(value);
+  parseStarBattleRows(constraints, rows, cols);
+  console.log(constraints);
 };
 
 const PARSE_PUZZLE = {
