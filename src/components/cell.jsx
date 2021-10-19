1f448ee0ae6956b4c43ed1ff2ef331bb9fef2a04
Robert King <robert.o.king@hotmail.co.uk>
2021-10-19 23:25:15 +0100

    Added: error class to cells when appropriate which colours <Star>s red

--- src/components/cell.jsx
+++ src/components/cell.jsx
@@ -14,7 +14,7 @@ export const Cell = ({ i, j, id, contents, cellSize, mouseDownHandler, mouseOver
   }
 
   return (
-    <g>
+    <g className={contents.errors.length > 0 && 'error'}>
       {contents?.entry === 'star' &&
         (<Star
           x={(i+0.5) * cellSize} y={(j+0.5) * cellSize}
