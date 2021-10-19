0c0eb2cd8207c1a4c5f1b12a996bfb340ba4adaf
Robert King <robert.o.king@hotmail.co.uk>
2021-10-19 15:20:32 +0100

    Refactor: <playerUI> to use id and flat array for cells

--- src/components/playerUI.jsx
+++ src/components/playerUI.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { idToIJ } from '../helpers/idToIJ.js';
-
 import { useEventListener } from '../hooks/useEventListener.js';
 
 const INPUT_TO_ENTRY = {
@@ -9,18 +7,18 @@ const INPUT_TO_ENTRY = {
   '2': {'centres': 'star'}
 };
 
-const updateCellEntry = (cells, i, j, newEntry) => {
+const updateCellEntry = (cells, id, newEntry) => {
   if ('entry' in newEntry) {
-    if (cells[j][i]['entry'] === newEntry['entry']) {
-      cells[j][i]['entry'] = '';
+    if (cells[id]['entry'] === newEntry['entry']) {
+      cells[id]['entry'] = '';
     } else {
-      cells[j][i]['entry'] = newEntry['entry'];
+      cells[id]['entry'] = newEntry['entry'];
     }
   } else if ('centres' in newEntry) {
-    if (cells[j][i]['centres'].includes(newEntry['centres'])) {
-      cells[j][i]['centres'] = cells[j][i]['centres'].filter(entry => entry !== newEntry['centres']);
+    if (cells[id]['centres'].includes(newEntry['centres'])) {
+      cells[id]['centres'] = cells[id]['centres'].filter(entry => entry !== newEntry['centres']);
     } else {
-      cells[j][i]['centres'].push(newEntry['centres']);
+      cells[id]['centres'].push(newEntry['centres']);
     }
   }
 };
@@ -34,8 +32,7 @@ export const PlayerUI = ({ selecteds, cells, setCells, cols, checkErrors }) => {
       const newEntry = INPUT_TO_ENTRY[key];
 
       selecteds.forEach((id) => {
-        const [i, j] = idToIJ(id, cols);
-        updateCellEntry(newCells, i, j, newEntry);
+        updateCellEntry(newCells, id, newEntry);
       });
       checkErrors(newCells, selecteds);
       setCells(newCells);
