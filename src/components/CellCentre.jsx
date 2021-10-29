a83041a47fa4ce727d458ba3cd279dbd103b3f2f
Robert King <robert.o.king@hotmail.co.uk>
2021-10-29 22:53:24 +0100

    Refactored: <CellCentre> to cope with not being passed an array

--- src/components/CellCentre.jsx
+++ src/components/CellCentre.jsx
@@ -3,10 +3,18 @@ import React from 'react';
 import { CellContents } from './CellContents.jsx';
 
 export const CellCentre = ({ centres, x, y, cellSize }) => {
+  const contents = () => {
+    if (Array.isArray(centres)) {
+      return centres[0];
+    } else {
+      return centres;
+    }
+  }
+
   return (
     <g>
       <CellContents
-          contents={centres[0]}
+          contents={contents()}
           x={x} y={y}
           size={cellSize * 0.2}
           />
