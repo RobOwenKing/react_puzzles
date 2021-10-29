9eafeec1720911ca2e3417691b71c9608d07380c
Robert King <robert.o.king@hotmail.co.uk>
2021-10-29 16:56:03 +0100

    Refactored: helpers/handleKeyPress.js created

--- src/components/PlayerUI.jsx
+++ src/components/PlayerUI.jsx
@@ -1,82 +1,18 @@
 import React from 'react';
 
-import { idToIJ } from '../helpers/idToIJ.js';
+import { INPUT_TO_ENTRY, handleKeyPress, handleArrowPress } from '../helpers/handleKeyPress.js';
 
 import { useEventListener } from '../hooks/useEventListener.js';
 
-const INPUT_TO_ENTRY = {
-  '1': {'entry': 'star'},
-  '2': {'centres': 'star'},
-  '3': {'entry': 'dot'}
-};
-
-const updateCellEntry = (cells, id, newEntry) => {
-  if ('entry' in newEntry) {
-    if (cells[id]['entry'] === newEntry['entry']) {
-      cells[id]['entry'] = '';
-    } else {
-      cells[id]['entry'] = newEntry['entry'];
-    }
-  } else if ('centres' in newEntry) {
-    if (cells[id]['centres'].includes(newEntry['centres'])) {
-      cells[id]['centres'] = cells[id]['centres'].filter(entry => entry !== newEntry['centres']);
-    } else {
-      cells[id]['centres'].push(newEntry['centres']);
-    }
-  }
-};
-
-const calculateSelectedCell = (key, selecteds, rows, cols) => {
-  const lastAddedID = selecteds[selecteds.length - 1];
-  let idToAdd = lastAddedID;
-  const [i, j] = idToIJ(lastAddedID, cols);
-
-  if (key === 'ArrowUp') {
-    idToAdd += j === 0 ? (rows-1) * cols : -cols;
-  } else if (key === 'ArrowDown') {
-    idToAdd += j === rows-1 ? (1-rows) * cols : cols;
-  } else if (key === 'ArrowLeft') {
-    idToAdd += i === 0 ? cols-1 : -1;
-  } else if (key === 'ArrowRight') {
-    idToAdd += i === cols-1 ? 1-cols : 1;
-  }
-
-  return idToAdd;
-};
-
 export const PlayerUI = ({ selecteds, setSelecteds, cells, setCells, rows, cols, checkErrors }) => {
-  const handleEntryKey = (key) => {
-    const newCells = [...cells];
-    const newEntry = INPUT_TO_ENTRY[key];
-
-    selecteds.forEach((id) => {
-      updateCellEntry(newCells, id, newEntry);
-    });
-    checkErrors(newCells, selecteds);
-    setCells(newCells);
-  };
-
-  const handleArrow = (e, key) => {
-    let newSelecteds = [...selecteds];
-    const newID = calculateSelectedCell(key, selecteds, rows, cols);
-
-    if (e.ctrlKey || e.shiftKey) {
-      newSelecteds.push(newID);
-    } else {
-      newSelecteds = [newID];
-    }
-
-    setSelecteds(newSelecteds);
-  };
-
   const inputHandler = (e) => {
     e.preventDefault();
     const key = e.key;
 
     if (key in INPUT_TO_ENTRY) {
-      handleEntryKey(key);
+      handleKeyPress(key, cells, selecteds, checkErrors, setCells);
     } else if (['ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'].includes(key)) {
-      handleArrow(e, key);
+      handleArrowPress(e, key, selecteds, setSelecteds, rows, cols);
     }
   };
 
