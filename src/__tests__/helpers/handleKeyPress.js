10300a1a8ff97b1e6cf7f71eecdef240acaed5db
Robert King <robert.o.king@hotmail.co.uk>
2021-11-11 15:41:14 +0000

    Added: Tests for handleKeyPress()

--- src/__tests__/helpers/handleKeyPress.js
+++ src/__tests__/helpers/handleKeyPress.js
@@ -10,4 +10,32 @@ describe('handleKeyPress()', () => {
     expect(sample).toBeInstanceOf(Array);
     expect(sample.length).toBe(cells.length);
   });
+  it('should add entry correctly when passed one selected cell', () => {
+    const sample = handleKeyPress('1', cells, [0], checkErrors);
+    cells[0]['entry'] = 'star';
+    expect(sample).toStrictEqual(cells);
+
+    cells[0]['entry'] = '';
+  });
+  it('should add entries correctly when passed multiple selected cells', () => {
+    const sample = handleKeyPress('1', cells, [0, 1, 5], checkErrors);
+    cells[0]['entry'] = 'star';
+    cells[1]['entry'] = 'star';
+    cells[5]['entry'] = 'star';
+    expect(sample).toStrictEqual(cells);
+
+    cells[0]['entry'] = '';
+    cells[1]['entry'] = '';
+    cells[5]['entry'] = '';
+  });
+  it('should remove an entry when user passes same again', () => {
+    const temp = handleKeyPress('1', cells, [2], checkErrors);
+    const sample = handleKeyPress('1', temp, [2], checkErrors);
+    cells[2]['entry'] = '';
+    expect(sample).toStrictEqual(cells);
+  });
+  /**
+    * @todo Tests for centres and corners (once implemented fully)
+    * @todo Tests for switching mode (eg: colours to numbers to etc)
+  */
 });
