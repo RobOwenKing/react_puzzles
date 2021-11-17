6e915a8e018a50fa47a4855b4b5e4b0b444778df
Robert King <robert.o.king@hotmail.co.uk>
2021-11-17 10:47:10 +0000

    Added: Start of expanded setCells() to enable undo

--- src/hooks/useCells.js
+++ src/hooks/useCells.js
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import { createCells } from '../helpers/create2DArray.js';
 
 export const useCells = (rows, cols) => {
-  const [cells, setCells] = useState(createCells(rows, cols));
+  const [currentCells, setCurrentCells] = useState(createCells(rows, cols));
+  const undoQueue = useRef([]);
+  const diffForUndoQueue = useRef([]);
 
-  return [cells, setCells];
+  const setCells = (newCells) => {
+    if (diffForUndoQueue.current.length > 0) { undoQueue.current.push(diffForUndoQueue.current); }
+
+    diffForUndoQueue.current = [];
+    currentCells.forEach((cell, index) => {
+      /**
+        * @todo Improve below comparison
+      */
+      if (cell !== newCells[index]) { diffForUndoQueue.current.push(cell); }
+    })
+
+    setCurrentCells(newCells);
+  }
+
+  return [currentCells, setCells];
 }
