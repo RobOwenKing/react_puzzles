e6613a3108e497ed5e0628c9e9388b0c70e86906
Robert King <robert.o.king@hotmail.co.uk>
2021-11-17 10:55:02 +0000

    Refactored: diffForUndoQueue useRef unnecessary plus improved comparison of when to push cells to undoQueue

--- src/hooks/useCells.js
+++ src/hooks/useCells.js
@@ -5,19 +5,20 @@ import { createCells } from '../helpers/create2DArray.js';
 export const useCells = (rows, cols) => {
   const [currentCells, setCurrentCells] = useState(createCells(rows, cols));
   const undoQueue = useRef([]);
-  const diffForUndoQueue = useRef([]);
 
   const setCells = (newCells) => {
-    if (diffForUndoQueue.current.length > 0) { undoQueue.current.push(diffForUndoQueue.current); }
+    const diffForUndoQueue = [];
 
     diffForUndoQueue.current = [];
     currentCells.forEach((cell, index) => {
-      /**
-        * @todo Improve below comparison
-      */
-      if (cell !== newCells[index]) { diffForUndoQueue.current.push(cell); }
+      const newCell = newCells[index];
+      if (cell.entry !== newCell.entry ||
+          cell.centres.length !== newCell.centres.length ||
+          cell.errors.length !== newCell.errors.length
+      ) { diffForUndoQueue.current.push(cell); }
     })
 
+    undoQueue.current.push(diffForUndoQueue.current);
     setCurrentCells(newCells);
   }
 
