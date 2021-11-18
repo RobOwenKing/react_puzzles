7f22fee4207c25ee4b36f67f3c27ed8b812339c7
Robert King <robert.o.king@hotmail.co.uk>
2021-11-18 15:07:09 +0000

    Added: useCells test of cells

--- src/__tests__/hooks/useCells.js
+++ src/__tests__/hooks/useCells.js
@@ -1,11 +1,15 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
 import { useCells } from '../../hooks/useCells.js';
 
 import { createCells } from '../../helpers/create2DArray.js';
 
-describe('cells', () => {
-  it('should return the same as createCells() when first called with same values', () => {
-    const [cells, ...rest] = useCells(3, 4);
-    const sample = createCells(3, 4);
-    expect(cells).toStrictEqual(sample);
+describe('useCells', () => {
+  describe('cells', () => {
+    it('should return the same as createCells() when first called with same values', () => {
+      const { result } = renderHook(() => useCells(3, 4));
+      const sample = createCells(3, 4);
+      expect(result.current.cells).toStrictEqual(sample);
+    });
   });
 });
