cca7e2157d9d3e03d59d984ca9f650d11bedf0c8
Robert King <robert.o.king@hotmail.co.uk>
2021-11-23 17:06:30 +0000

    Added: redo() passing 2/4 tests

--- src/hooks/useCells.js
+++ src/hooks/useCells.js
@@ -5,6 +5,7 @@ import { createCells } from '../helpers/create2DArray.js';
 export function useCells(rows, cols) {
   const [currentCells, setCurrentCells] = useState(createCells(rows, cols));
   const undoQueue = useRef([]);
+  const redoQueue = useRef([]);
 
   const _isMounted = useRef(true);
 
@@ -44,18 +45,44 @@ export function useCells(rows, cols) {
   const undo = () => {
     if (!canUndo()) { return; }
 
-    const diff = undoQueue.current.pop();
+    const undoDiff = undoQueue.current.pop();
+    const redoDiff = [];
 
     const newCells = copyOfCells().map((cell) => {
-      if (diff[0] && diff[0].id === cell.id) {
-        return diff.shift();
+      if (undoDiff[0] && undoDiff[0].id === cell.id) {
+        redoDiff.push(cell);
+        return undoDiff.shift();
       } else {
         return cell;
       }
     });
 
-    if (_isMounted.current) { setCurrentCells(newCells); }
+    if (_isMounted.current) {
+      setCurrentCells(newCells);
+      redoQueue.current.push(redoDiff);
+    }
   };
 
-  return { cells: currentCells, copyOfCells, setCells, canUndo, undo };
+  const redo = () => {
+    // if (!canUndo()) { return; }
+
+    const undoDiff = [];
+    const redoDiff = redoQueue.current.pop();
+
+    const newCells = copyOfCells().map((cell) => {
+      if (redoDiff[0] && redoDiff[0].id === cell.id) {
+        undoDiff.push(cell);
+        return redoDiff.shift();
+      } else {
+        return cell;
+      }
+    });
+
+    if (_isMounted.current) {
+      setCurrentCells(newCells);
+      undoQueue.current.push(redoDiff);
+    }
+  };
+
+  return { cells: currentCells, copyOfCells, setCells, canUndo, undo, redo };
 }
